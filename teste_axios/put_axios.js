9efc1b63fa216bbe139f848c7c6828397587e72c
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-21 03:32:31 +0530

    Usando o metodo Put para atualizar dados

--- teste_axios/put_axios.js
+++ teste_axios/put_axios.js
@@ -1,13 +1,35 @@
 const axios = require('axios')
 
-const id = 1
-axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`,{
-    title:"Novo titulo atualizado"
-} )
-.then(res=>{
-    console.log("dados", res.data.userId)
-    console.log(res.data)
-})
-.catch(e => {
-    console.error("Erro: ", e)
-})
+// const id = 1
+// const dadosAtualizados = {
+//     title:"Novo titulo atualizado",
+//     body: 'novo dado',
+//     userId:1
+// }
+// axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`, dadosAtualizados)
+// .then(res=>{
+//     console.log("dados: ", res.data.userId)
+//     console.log(res.data)
+// })
+// .catch(e => {
+//     console.error("Erro: ", e)
+// })
+
+//usando async / await
+
+async function atualizarDados() {
+    try{
+        const id = 1
+        const novoDados = {
+            title: "Lendo livro",
+            body: 'Programar em JavaScript',
+            userId: 1
+        }
+        const res = await axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`, novoDados)
+        console.log("Novo dados: ", res.data)
+    }catch(e){
+        console.error('Erro: ', e.message)
+    }
+    
+}
+atualizarDados()
