5a5b2d1839a1051ad8252cf54ac41a790cba43be
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-20 23:04:59 +0530

    eliminamndo comentarios

--- teste_axios/axio.js
+++ teste_axios/axio.js
@@ -1,13 +1,13 @@
 // import axios  from "axios";
 const axios =  require('axios')
 
-// axios.get('https://jsonplaceholder.typicode.com/posts/1')
-// .then(function(res) {
-//     console.log("Dados: ", res.data)
-// })
-// .catch(function(e){
-//     console.error("Erro: ", e.message)
-// })
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+.then(function(res) {
+    console.log("Dados: ", res.data)
+})
+.catch(function(e){
+    console.error("Erro: ", e.message)
+})
 
 // usando async / await
 
