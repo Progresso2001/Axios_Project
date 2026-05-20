42ae04dc0220ede9de9ce072d8215f508e24605b
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-21 03:53:54 +0530

    Usando o metodo Delete

---
+++ teste_axios/deletar_axios.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+
+const id = 1
+axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+.then(res=>console.log("Deletado com sucesso\nstatus:", res.status))
+.catch(e=>console.error("Erro ao deletar: ", e.message))
+
+// usando async /await
+
+async function deletarPost(){
+    try{
+        const id = 33333333333
+        const res= await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        console.log("Deletado com sucesso\nstatus: ", res.status)
+    }catch(e){
+        console.log("Erro: ", e.message)
+    }
+}
+deletarPost()
