36160b89ef9e37f074d877f4be15bf91e9f0fd8b
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-22 16:51:26 +0530

    Configurar o stringfy

--- main.js
+++ main.js
@@ -7,18 +7,19 @@ async function testarConhecimentoApi(){
         
         console.log("Dados da api VIACEP: ", res.status)
 
-        // console.log("Status: ", res.status)
-        // console.log("Requisicão com sucesso: ", res.statusText)
-        // console.log(res)
+        console.log("Status: ", res.status)
+        console.log("Requisicão com sucesso: ", res.statusText)
+        console.log(res)
     }
     catch(error){
     // tratamento de erro usando a funcao includes()
 
-    //    if(error.message.includes('Request failed')){
-    //      console.error("Verificar o link: ", error.status)
-    //    }else{
-    //      console.error("Erro: ", error)
-    //    }
+       if(error.message.includes('Request failed')){
+         console.error("Verificar o link: ", error.status)
+       }else{
+         console.error("Erro: ", error)
+       }
+
     // tratamento de erro usando o status
         if(error.status === 404){
             console.error("Alerta Verifique a requisição: ", error.statusText)
@@ -29,6 +30,7 @@ async function testarConhecimentoApi(){
 }
 testarConhecimentoApi()
 
+// -------------------------------------TESTANDO OBJETOS-------------------------------------------------------------
 const infPessoal = {
     id: 1,
     nome: "Joaquim",
@@ -45,18 +47,18 @@ const infPessoal = {
 async function transformarEmJson(){
     // impressao de dados sem tratamento de erros
 
-    const res = await JSON.stringify(infPessoal)
+    const res = await JSON.stringify(infPessoal, null, 2)
     console.log("Dados em Json: ", res)
     
     // imprimir com tratamento de erro
 
-    const myFyle =  JSON.stringify(infPessoal)
-    const res = await myFyle
-    if(typeof(res)=== 'string'){
-        console.log("Dados transformado em json: ", res)
-    }else{
-        console.log("Erro ao transformar em json o objecto")
+    // const myFyle =  JSON.stringify(infPessoal)
+    // const res = await myFyle
+    // if(typeof(res)=== 'string'){
+    //     console.log("Dados transformado em json: ", res)
+    // }else{
+    //     console.log("Erro ao transformar em json o objecto")
 
-    }
+    // }
 }
 transformarEmJson()
