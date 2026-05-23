e289d71602969505192d2ab56c27e6b074e17568
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-23 19:17:27 +0530

    Usando async e await

--- function_axios/function.js
+++ function_axios/function.js
@@ -25,3 +25,26 @@ promise
     })
 setTimeout(()=> cancelar(), 2000)
 
+// Usando async e await
+
+
+const controller = new AbortController()
+
+async function carregarDados(){
+    try{
+        const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1', 
+            {
+        signal: controller.signal
+    })
+    console.log(res.data)
+    }catch(e){
+        if(e.name ==='CanceledError' || e.name=== 'AborError'){
+            console.log('Requisição abortada na saida')
+        }else{
+            console.error(e)
+        }
+    }
+}
+carregarDados()
+
+setTimeout(()=>controller.abort(), 2000)
